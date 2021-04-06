d28fdd5f7143547e640828dd6a2571eb90d83eee
Iria <imiguez@gradiant.org>
2021-04-06 06:53:27 +0000

    Upgraded to Open5gs 2.2.3

--- docs/register_subscriber.sh
+++ docs/register_subscriber.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NAMESPACE=default
+NAMESPACE=test
 POD_NAME=`kubectl -n $NAMESPACE get pod --selector=app.kubernetes.io/name=mongodb -o jsonpath='{.items[*].metadata.name}'`
 
 #IMSI_LIST="901700000039226 "
@@ -12,6 +12,5 @@ OPC=E8ED289DEBA952E4283B54E88E6183CA
 
 for IMSI in $IMSI_LIST; do
 kubectl -n $NAMESPACE exec $POD_NAME -- /bin/bash -c \
-"mongo --eval \"db.subscribers.update({\\\"imsi\\\" : \\\"$IMSI\\\"}, { \\\$setOnInsert: { \\\"imsi\\\" : \\\"$IMSI\\\", \\\"pdn\\\" : [ { \\\"apn\\\" : \\\"internet\\\", \\\"_id\\\" : new ObjectId(), \\\"pcc_rule\\\" : [ ], \\\"ambr\\\" : { \\\"downlink\\\" : NumberLong(1024000), \\\"uplink\\\" : NumberLong(1024000) }, \\\"qos\\\" : { \\\"qci\\\" : NumberInt(9), \\\"arp\\\" : { \\\"priority_level\\\" : NumberInt(8), \\\"pre_emption_vulnerability\\\" : NumberInt(1), \\\"pre_emption_capability\\\" : NumberInt(0) } }, \\\"type\\\" : NumberInt(0) } ], \\\"ambr\\\" : { \\\"downlink\\\" : NumberLong(1024000), \\\"uplink\\\" : NumberLong(1024000) }, \\\"subscribed_rau_tau_timer\\\" : NumberInt(12), \\\"network_access_mode\\\" : NumberInt(2), \\\"subscriber_status\\\" : NumberInt(0), \\\"access_restriction_data\\\" : NumberInt(32), \\\"security\\\" : { \\\"k\\\" : \\\"$KI\\\", \\\"amf\\\" : \\\"8000\\\", \\\"op\\\" : null, \\\"opc\\\" : \\\"$OPC\\\" }, \\\"__v\\\" : 0 } }, upsert=true);\" open5gs"
-
+"mongo --eval \"db.subscribers.update({\\\"imsi\\\" : \\\"$IMSI\\\"}, { \\\$setOnInsert: { \\\"imsi\\\" : \\\"$IMSI\\\", \\\"subscribed_rau_tau_timer\\\" : NumberInt(12), \\\"network_access_mode\\\" : NumberInt(2), \\\"subscriber_status\\\" : NumberInt(0), \\\"access_restriction_data\\\" : NumberInt(32),  \\\"slice\\\" : [ { \\\"sst\\\": NumberInt(1), \\\"default_indicator\\\": true, \\\"session\\\": [{ \\\"name\\\": \\\"internet\\\", \\\"type\\\": NumberInt(3), \\\"pcc_rule\\\": [], \\\"ambr\\\": {\\\"uplink\\\": {\\\"value\\\": NumberInt(1), \\\"unit\\\": NumberInt(0)}, \\\"downlink\\\": {\\\"value\\\": NumberInt(1), \\\"unit\\\": NumberInt(0) } }, \\\"qos\\\": { \\\"index\\\": NumberInt(9), \\\"arp\\\": {\\\"priority_level\\\": NumberInt(8), \\\"pre_emption_capability\\\": NumberInt(1), \\\"pre_emption_vulnerability\\\": NumberInt(1)} } } ] } ],  \\\"ambr\\\": {\\\"uplink\\\": {\\\"value\\\": NumberInt(1), \\\"unit\\\": NumberInt(0)}, \\\"downlink\\\": {\\\"value\\\": NumberInt(1), \\\"unit\\\": NumberInt(0) } }, \\\"security\\\" : { \\\"k\\\" : \\\"$KI\\\", \\\"amf\\\" : \\\"8000\\\", \\\"op\\\" : null, \\\"opc\\\" : \\\"$OPC\\\" }, \\\"__v\\\" : 0 } }, upsert=true);\" open5gs "
 done
