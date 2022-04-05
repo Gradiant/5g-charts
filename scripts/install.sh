82b708bdafdddac5ff27177a6578db7292c5c749
Carlos Giraldo <cgiraldo@gradiant.org>
2022-04-05 10:45:53 +0200

    add support for testing charts with default service type=Loadbalancer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+echo "Deploying kind cluster"
 kind create cluster --name charttest --wait 1m
+echo "Deploying metallb loadBalancer"
+kind_IP=$(docker network inspect -f '{{index (index .IPAM.Config 0) "Gateway" }}' kind)
+kubectl --kubeconfig=$kind_kubeconfig apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl --kubeconfig=$kind_kubeconfig apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+cat <<EOF | kubectl apply --kubeconfig=$kind_kubeconfig -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - ${kind_IP}00-${kind_IP}20
+EOF
 
 docker run -ti --rm \
   --net=host \
@@ -11,4 +30,5 @@ docker run -ti --rm \
   --workdir=/gradiant quay.io/helmpack/chart-testing ct install \
      --chart-repos bitnami=https://charts.bitnami.com/bitnami $@
 
+echo "deleting kind cluster"
 kind delete cluster --name charttest
