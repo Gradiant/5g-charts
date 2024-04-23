6aa457314d1020ae910dff9d728d29824afc0f27
M S Sooraj <srjsubramanian@gmail.com>
2024-04-23 13:56:20 +0530

    remove resource limits but set requests

--- custom/add_subscribers.js
+++ custom/add_subscribers.js
@@ -1,4 +1,8 @@
-slice_data = [
+const mongoose = require("mongoose");
+
+const DB_URI = "mongodb://open5gs-release-mongodb/open5gs";
+
+const slice_data_template = [
   {
     sst: 1,
     default_indicator: true,
@@ -24,14 +28,14 @@ slice_data = [
   },
 ];
 
-sub_data = {
+const subscriber_template = {
   imsi: "999700000000001",
   subscribed_rau_tau_timer: 12,
   network_access_mode: 0,
   subscriber_status: 0,
   operator_determined_barring: 0,
   access_restriction_data: 32,
-  slice: slice_data,
+  slice: slice_data_template,
   ambr: { uplink: { value: 1, unit: 3 }, downlink: { value: 1, unit: 3 } },
   security: {
     k: "465B5CE8 B199B49F AA5F0A2E E238A6BC",
@@ -43,9 +47,79 @@ sub_data = {
   __v: 0,
 };
 
-create_subscribers_with_imsi = (number, start_imsi = 999700000000001) => {
+const create_subscribers_with_imsi = (number, start_imsi = 999700000000001) => {
   const subscribers = Array.from(Array(number)).map((_, index) => {
-    return { ...sub_data, imsi: (start_imsi + index).toString() };
+    return { ...subscriber_template, imsi: (start_imsi + index).toString() };
   });
   return subscribers;
 };
+
+const SliceSessionSchema = new mongoose.Schema({
+  name: String,
+  type: Number,
+  pcc_rule: [],
+  ambr: {
+    uplink: { value: Number, unit: Number },
+    downlink: { value: Number, unit: Number },
+  },
+  qos: {
+    index: Number,
+    arp: {
+      priority_level: Number,
+      pre_emption_capability: Number,
+      pre_emption_vulnerability: Number,
+    },
+  },
+});
+
+const SliceSchema = new mongoose.Schema({
+  sst: Number,
+  default_indicator: Boolean,
+  session: [SliceSessionSchema],
+});
+
+const SubscriberSchema = new mongoose.Schema({
+  imsi: String,
+  subscribed_rau_tau_timer: Number,
+  network_access_mode: Number,
+  subscriber_status: Number,
+  operator_determined_barring: Number,
+  access_restriction_data: Number,
+  slice: [SliceSchema],
+  ambr: {
+    uplink: { value: Number, unit: Number },
+    downlink: { value: Number, unit: Number },
+  },
+  security: {
+    k: String,
+    amf: String,
+    opc: String,
+  },
+  schema_version: Number,
+  __v: Number,
+});
+
+const save_subscribers = async (subscribers) => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log("Db Connected");
+    const SubscriberModel = mongoose.model("subscribers", SubscriberSchema);
+    try {
+      const result = await SubscriberModel.insertMany(subscribers);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+      console.error("error bulk saving");
+    } finally {
+      mongoose.disconnect();
+    }
+  } catch (error) {
+    console.error(error);
+    console.error("Unable to connect to the DB");
+  }
+};
+
+const subscribers = create_subscribers_with_imsi(50000, 999700000070001);
+console.log(subscribers);
+
+save_subscribers(subscribers);
