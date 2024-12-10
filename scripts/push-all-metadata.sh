9a6676f87c7f74b837cae603729174706a54cffd
Carlos Giraldo <cgiraldo@gradiant.org>
2024-12-10 12:02:45 +0100

    update artifacthub metadata

--- scripts/push-all-metadata.sh
+++ scripts/push-all-metadata.sh
@@ -6,14 +6,16 @@ push_metadata () {
   oras push \
   $REGISTRY/$1:artifacthub.io \
   --config /dev/null:application/vnd.cncf.artifacthub.config.v1+yaml \
-  charts/$1/artifacthub-repo.yml:application/vnd.cncf.artifacthub.repository-metadata.layer.v1.yaml
+  artifacthub-repo.yml:application/vnd.cncf.artifacthub.repository-metadata.layer.v1.yaml
 }
 
 docker login
 
 for dir in charts/*/ 
 do
-    dir=${dir%*/}      # remove the trailing "/"
-    dir="${dir##*/}"    # print everything after the final "/"
-    push_metadata $dir
+    pushd $dir
+    chart_name=${dir%*/}      # remove the trailing "/"
+    chart_name="${chart_name##*/}"    # print everything after the final "/"
+    push_metadata $chart_name
+    popd
 done
